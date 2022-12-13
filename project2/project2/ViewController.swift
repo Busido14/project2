57a7f76b8cd768b30ad5b2f2b8779712f1db2367
Artem <artchzhen@gmail.com>
2022-12-13 15:08:40 +0500

    initial commit develop

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Артем Чжен on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
